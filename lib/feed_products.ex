e95eeee64a98c9bd0ad04c631dd2d9e5ea192adb
Francisco José Fernández de Pedro <fjfdepedro@gmail.com>
2018-12-28 08:45:42 +0100

    Create elixir project

---
+++ lib/feed_products.ex
@@ -0,0 +1,18 @@
+defmodule FeedProducts do
+  @moduledoc """
+  Documentation for FeedProducts.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FeedProducts.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/feed_products_test.exs
@@ -0,0 +1,8 @@
+defmodule FeedProductsTest do
+  use ExUnit.Case
+  doctest FeedProducts
+
+  test "greets the world" do
+    assert FeedProducts.hello() == :world
+  end
+end
